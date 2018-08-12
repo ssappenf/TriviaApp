2816448e66ae183ad5530f708d2be367a7d7e079
Sam Sappenfield <ssappenf@mit.edu>
2018-08-11 23:04:10 -0700

    brainstorming in comments

--- Trivia/Trivia/QuestionManager.swift
+++ Trivia/Trivia/QuestionManager.swift
@@ -19,9 +19,9 @@ struct Question{
     func newQuestion() -> Question{
        let url = URL(string: "https://opentdb.com/api.php?amount=1&category=9&difficulty=easy&type=multiple")
         var returnValue=Question(category: "", question: "", correctAnswer: "", possibleAnswers:[ " "], difficulty: "", type: "")
-        let configuration=URLSessionConfiguration.ephemeral//figure out what this does
+        let configuration=URLSessionConfiguration.ephemeral
         let session=URLSession(configuration: configuration, delegate: nil, delegateQueue: OperationQueue.main)
-        //reps actual connection with server
+        //reps actual connection with server//whats the delegate for?
         let task = session.dataTask(with: url!) { (data, response, error) in
            
             if let data = data {
@@ -29,33 +29,37 @@ struct Question{
                     // Convert the data to JSON
                     let jsonSerialized = try JSONSerialization.jsonObject(with: data, options: []) as? [String : Any]
                     
-                    if let json = jsonSerialized{
-                         let results=json["results"] as! NSArray
+                    if let json = jsonSerialized{//parse the json data
+                         let results=json["results"] as! NSArray//json ["results"] is an ns object i think that contains dictionary// should double check
                          let resultsDict=results[0] as! NSDictionary
                         let tempCat = resultsDict["category"] as! String
                         let tempQ = resultsDict["question"] as! String
                         let tempCA=resultsDict["correct_answer"] as! String
                          var tempPA=resultsDict["incorrect_answers"] as! [String]
-                        tempPA.insert(tempCA, at: Int(arc4random_uniform(UInt32(2))))
+                        tempPA.insert(tempCA, at: Int(arc4random_uniform(UInt32(2))))//randomly place the correct answer amon the remaining answers
                          let tempDif=resultsDict["difficulty"] as! String
-                         let tempType=resultsDict["type"] as! String
+                         let tempType=resultsDict["type"] as! String//muliple choice vs truefalse
+                        //eventually need to deal with symbols being messed up
                         returnValue = Question(category: tempCat, question: tempQ, correctAnswer: tempCA, possibleAnswers: tempPA, difficulty: tempDif, type: tempType)
                     print(returnValue)//this should be printing first but its printing after
                         //need to break here to exit the loop maybe?idk//cant return since void function can i make it nonvoid?
                     //ask poonam or somebody about completions/internet says they work but not on xcode anymore
-                }
+                        
+                        }
                    
-            }  catch let error as NSError {
-                print(error.localizedDescription)
+                    }  catch let error as NSError {
+                        print(error.localizedDescription)
+                        }
                 
-                }}
-         else if let error = error {
-            print(error.localizedDescription)
+            }
+            else if let error = error {
+                print(error.localizedDescription)
                
-        }
+            }
     
         }
-        task.resume()//is this why its looping back afterward?
+        
+        task.resume()//is this why its looping back afterward?still unclear on what excatly resume does//swift doc says resumes task if its been halted so does the request get halt/resumes/returns the empty value/then fills value//why is task being halted
         //RunLoop.main.run()
         print(returnValue)//why is this going before the previous print
         print("gotHere")
@@ -73,16 +77,19 @@ struct Question{
         }
     }
 }
+
 enum GameStatus{
     case notStarted
     case playing
     case gameOver
 }
+
 struct GameState{
     let gameStatus:GameStatus
     let score:Int
     let currentQuestion:Question?
 }
+
 class GameMachine{
     var gameState:GameState
     
@@ -100,6 +107,7 @@ class GameMachine{
     func newQuestion(){
         let test=Question(category: "", question: "", correctAnswer: "", possibleAnswers:[ " "], difficulty: "", type: "")
         gameState=GameState(gameStatus: .playing, score: gameState.score, currentQuestion: test.newQuestion() )
+        //cant remember how to do this without test//google tomorrow
         
     }
     func answeredQuestion(answer:String){
@@ -110,9 +118,9 @@ class GameMachine{
             
         }
         else{
-            gameState=GameState(gameStatus: .gameOver, score: gameState.score, currentQuestion: nil)
+            gameState=GameState(gameStatus: .gameOver, score: gameState.score, currentQuestion: nil)//maybe instead switch players
         }
     }
-    //maybe set it up so it could be multiplayer
+    //maybe set it up so it could be multiplayer//go back and forth between two players until one gets three wrong maybe or maybe withtin some time limit
 }
 
